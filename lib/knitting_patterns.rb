5aaa338ece4c4c68bb41e6000a4a861d157bb663
Ashley Bland <ashleyjbland@gmail.com>
2018-11-14 22:58:30 +0000

    changes to gemspec

--- lib/knitting_patterns.rb
+++ lib/knitting_patterns.rb
@@ -1,5 +1,5 @@
 #this is my environment
-
+require "nokogiri"
 require_relative './knitting_patterns/version'
 require_relative './knitting_patterns/cli'
 require_relative './knitting_patterns/pattern'
