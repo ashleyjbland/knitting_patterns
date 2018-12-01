55cb1a468fea9c12bbcd4ce0018354ab663f8b73
Ashley Bland <ashleyjbland@gmail.com>
2018-12-01 20:24:26 +0000

    changes to cli and pattern

--- lib/knitting_patterns/cli.rb
+++ lib/knitting_patterns/cli.rb
@@ -2,33 +2,29 @@
 class KnittingPatterns::CLI
 
   def call
-    list_patterns
+    puts "Hello fellow knitter!"
+    list_categories
     menu
   end
 
-  def list_patterns
-    puts "Hello fellow knitter!"
+  def list_categories
     puts "Here are the categories of the free knitting patterns from Purl Soho:"
-    @categories = KnittingPatterns::Pattern.scrape_knit_categories
+    @categories = KnittingPatterns::Pattern.knit_categories
     @categories.each.with_index(1) {|category, index| puts "#{index}. #{category}"}
-    #@urls = KnittingPatterns::Pattern.selected_pattern
-    #@url.each.with_index(1) do |url, index|
-      #puts "#{index}. #{url}"
-    #end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "To see all the patterns in a category, enter the number of the category. Type 'list' to see them all again or 'exit' to leave."
-      #puts "Enter the number of the pattern you'd like more details on or 'list' to see them all again or 'exit' to leave."
+      puts "To see all the patterns in a category, enter the number of the category."
+      puts "Type 'list' to see them all again or 'exit' to leave."
       input = gets.strip.downcase
 
       if input.to_i > 0
-        #the_url = @urls[input.to_i-1]
-        #puts "#{the_url}"
+        puts "#{@categories[input.to_i-1]}"
       elsif input == "list"
-        list_patterns
+        puts "Interested in some more ideas, huh?"
+        list_categories
       elsif input == "exit"
         goodbye
       else
--- lib/knitting_patterns/pattern.rb
+++ lib/knitting_patterns/pattern.rb
@@ -1,30 +1,21 @@
 class KnittingPatterns::Pattern
   attr_accessor :title, :url, :category
 
-  def self.knit_patterns
-    #scrape ravelry
+  def self.knit_categories
     self.scrape_knit_categories
   end
 
-#  def self.scrape_patterns
-#    patterns = []
-#
-#    patterns << self.scrape_knit_categories
-#
-#    patterns
-#  end
-
   def self.scrape_knit_categories
-    #doc = Nokogiri::HTML(open("https://www.ravelry.com/patterns/search#view=captioned_thumbs&sort=recently-popular&craft=knitting"))
     doc = Nokogiri::HTML(open("https://www.purlsoho.com/create/category/knit/knit-view-all/"))
     category = doc.css("li.categories").css("ul").css("li").text.split("\n")
-    #.each.with_index(1) {|category, index| puts "#{index}. #{category.text}"}
     #pattern.title = doc.css("li").css("h3").each.with_index(1) {|title, index| puts "#{index}. #{title.text}"}
     #pattern.title = doc.css("li").css("h3").each {|title| puts title.text}
     #pattern.url = doc.css("li").css("h3").css("a").attr("href").each {|url| puts url}
 #binding.pry
     category
   end
+
+
 
 
 
