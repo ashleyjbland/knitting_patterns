5161d5ef29a79cb583d704372433bdc893eeb5f2
Ashley Bland <ashleyjbland@gmail.com>
2019-01-08 23:57:34 +0000

    changes to knitting_patterns.rb and added config

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative '../lib/knitting_patterns/version'
+require_relative '../lib/knitting_patterns/scraper'
+require_relative '../lib/knitting_patterns/pattern'
+require_relative '../lib/knitting_patterns/cli'
--- lib/knitting_patterns.rb
+++ lib/knitting_patterns.rb
@@ -1,11 +1,4 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-
-
 module KnittingPatterns
-  require_relative '../lib/knitting_patterns/version'
-  require_relative '../lib/knitting_patterns/scraper'
-  require_relative '../lib/knitting_patterns/pattern'
-  require_relative '../lib/knitting_patterns/cli'
 end
+
+require_relative '../config/environment'
